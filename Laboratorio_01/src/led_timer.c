f79f506d33c6b783f048ada8fd16924f5662a0a8
matheuscassarotti <86447493+matheuscassarotti@users.noreply.github.com>
2021-06-25 00:21:46 -0300

    Upload first project Laboratorio_01

---
+++ Laboratorio_01/src/led_timer.c
@@ -0,0 +1,42 @@
+#include <stdint.h>
+#include <stdbool.h>
+// includes da biblioteca driverlib
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+
+bool LED_D4 = 0;
+
+void main(void){
+  //SysCtlClockFreqSet(SYSCTL_OSC_INT | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_320, 24000000); // 24MHz
+  SysCtlClockFreqSet(SYSCTL_OSC_INT | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_320, 120000000); // 120MHz
+
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Habilita GPIO F (LED D3 = PF4, LED D4 = PF0)
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)); // Aguarda final da habilitação
+    
+  GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0); // LED D4 como saída
+  GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 1); // LED D4 aceso
+  GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_STRENGTH_12MA, GPIO_PIN_TYPE_STD);
+
+  SysTickIntEnable();
+  SysTickEnable();
+
+  while(1){
+      for(uint32_t timer = 0;timer<10000000;timer++){
+        if(timer > 9999998){
+          LED_D4 = !LED_D4;
+          GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, LED_D4);
+          
+          /* ###### ANÁLISE ########
+          24MHz - Baixa Otimização - Tempo estimado para 5 ciclos = 10,21s
+          120MHz - Baixa Otimização - Tempo estimado para 5 ciclos = 17,30s
+          24MHz - Alta Otimização - Tempo estimado para 5 ciclos = 4,11s
+          120MHz - Alta Otimização - Tempo estimado para 5 ciclos = 10,01s
+          ##########################
+          */
+          
+        } //end if
+      } //end for
+  } // while
+} // main
